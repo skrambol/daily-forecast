b12b4e9efdc7b29103852ba5713d268ca7e2c110
skrambol <systemplado@gmail.com>
2023-11-09 08:25:47 +0800

    fix: lint and formatter issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,67 @@
-import { useState } from 'react'
-import weatherService from './services/weather'
-import Forecast from './components/Forecast'
+import { useState } from "react";
+import weatherService from "./services/weather";
+import Forecast from "./components/Forecast";
 // import './App.css'
 
 function App() {
-  const [query, setQuery] = useState('')
-  const [cities, setCities] = useState([])
-  const [city, setCity] = useState({})
-  const [forecast, setForecast] = useState({})
+  const [query, setQuery] = useState("");
+  const [cities, setCities] = useState([]);
+  const [city, setCity] = useState({});
+  const [forecast, setForecast] = useState({});
 
   function searchCity(event) {
-    event.preventDefault()
+    event.preventDefault();
 
-    weatherService.citySearch(query)
-      .then(data => {
-        setCities(data)
-        setCity({})
-      })
+    weatherService.citySearch(query).then((data) => {
+      setCities(data);
+      setCity({});
+    });
   }
 
   function getForecast(city) {
-    weatherService.forecastFiveDays(city.Key)
-      .then(data => {
-        setForecast(data)
-        setCities([])
-        setCity(city)
-      })
+    weatherService.forecastFiveDays(city.Key).then((data) => {
+      setForecast(data);
+      setCities([]);
+      setCity(city);
+    });
   }
-
 
   return (
     <>
       <div>
         <form onSubmit={searchCity}>
-          <input value={query} onChange={e => setQuery(e.target.value)} placeholder='enter city'/>
-          <button type='submit'>search</button>
+          <input
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            placeholder="enter city"
+          />
+          <button type="submit">search</button>
         </form>
       </div>
       <ul>
-        {
-          cities.map(city => {
-            return (
-              <li key={city.Key}>
-                <span>{city.EnglishName}, </span>
-                <span>{city.AdministrativeArea.EnglishName} {city.AdministrativeArea.EnglishType} </span>
-                <span><i>({city.Region.EnglishName}, {city.Country.EnglishName}) </i></span>
-                <button onClick={() => getForecast(city)}>get five day forecast</button>
-              </li>
-            )
-          })
-        }
+        {cities.map((city) => {
+          return (
+            <li key={city.Key}>
+              <span>{city.EnglishName}, </span>
+              <span>
+                {city.AdministrativeArea.EnglishName}{" "}
+                {city.AdministrativeArea.EnglishType}{" "}
+              </span>
+              <span>
+                <i>
+                  ({city.Region.EnglishName}, {city.Country.EnglishName}){" "}
+                </i>
+              </span>
+              <button onClick={() => getForecast(city)}>
+                get five day forecast
+              </button>
+            </li>
+          );
+        })}
       </ul>
-      <Forecast forecast={forecast} city={city}/>
+      <Forecast forecast={forecast} city={city} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -1,34 +1,65 @@
-function Temperature({value, unit}) {
+function Temperature({ value, unit }) {
   return (
-    <span>{value}° {unit}</span>
-  )
+    <span>
+      {value}° {unit}
+    </span>
+  );
 }
 
-function Forecast({forecast, city}) {
-  if (Object.keys(forecast).length === 0) return null
+function Forecast({ forecast, city }) {
+  if (Object.keys(forecast).length === 0) return null;
 
   return (
     <div>
       <h2>
         <span>{city.EnglishName}, </span>
-        <span>{city.AdministrativeArea.EnglishName} {city.AdministrativeArea.EnglishType} </span>
-        <span><i>({city.Region.EnglishName}, {city.Country.EnglishName}) </i></span>
+        <span>
+          {city.AdministrativeArea.EnglishName}{" "}
+          {city.AdministrativeArea.EnglishType}{" "}
+        </span>
+        <span>
+          <i>
+            ({city.Region.EnglishName}, {city.Country.EnglishName}){" "}
+          </i>
+        </span>
       </h2>
-      {forecast.DailyForecasts.map(f => {
+      {forecast.DailyForecasts.map((f) => {
         return (
-          <div key={f.EpochDate} style={{ border: '1px solid black', margin: '1em', padding: '1em'}}>
+          <div
+            key={f.EpochDate}
+            style={{ border: "1px solid black", margin: "1em", padding: "1em" }}
+          >
             <p>Date: {f.Date}</p>
             <p>{f.Day.LongPhrase}</p>
-            <p>Temperature: <Temperature value={f.Temperature.Maximum.Value} unit={f.Temperature.Maximum.Unit}/> <Temperature value={f.Temperature.Minimum.Value} unit={f.Temperature.Minimum.Unit}/></p>
-            <p>RealFeel®: {f.RealFeelTemperature.Maximum.Phrase} <Temperature value={f.RealFeelTemperature.Maximum.Value} unit={f.RealFeelTemperature.Maximum.Unit}/> <Temperature value={f.RealFeelTemperature.Minimum.Value} unit={f.RealFeelTemperature.Minimum.Unit}/></p>
-            <p>Precipitation Probability: {f.Day.PrecipitationProbability}%</p>
             <p>
+              Temperature:{" "}
+              <Temperature
+                value={f.Temperature.Maximum.Value}
+                unit={f.Temperature.Maximum.Unit}
+              />{" "}
+              <Temperature
+                value={f.Temperature.Minimum.Value}
+                unit={f.Temperature.Minimum.Unit}
+              />
             </p>
+            <p>
+              RealFeel®: {f.RealFeelTemperature.Maximum.Phrase}{" "}
+              <Temperature
+                value={f.RealFeelTemperature.Maximum.Value}
+                unit={f.RealFeelTemperature.Maximum.Unit}
+              />{" "}
+              <Temperature
+                value={f.RealFeelTemperature.Minimum.Value}
+                unit={f.RealFeelTemperature.Minimum.Unit}
+              />
+            </p>
+            <p>Precipitation Probability: {f.Day.PrecipitationProbability}%</p>
+            <p></p>
           </div>
-        )
+        );
       })}
     </div>
-  )
+  );
 }
 
-export default Forecast
+export default Forecast;
